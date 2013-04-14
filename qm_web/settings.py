6a15fad028505383d6ef59ce692eca95a3af7ca8
xulin <mrmuxl@sina.com>
2013-04-14 20:53:35 +0800

    给kx_user表增加一个auto_id字段脚步,继续完成接口代码

--- qm_web/settings.py
+++ qm_web/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'kx3',                      # Or path to database file if using sqlite3.
+        'NAME': 'kx',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': 'mrmuxl',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
