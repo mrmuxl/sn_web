141e875119851a1d087882e26c85aab19f8f9f43
xulin <mrmuxl@sina.com>
2013-04-15 18:27:50 +0800

    拆分了用户注册登陆模块

--- kx/api/urls.py
+++ kx/api/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^record/$','kx.api.views.record'),
     url(r'^lan_record/$','kx.api.views.lan_record'),
+    url(r'^pub_record/$','kx.api.views.pub_record'),
     url(r'^uninstall/$','kx.api.views.uninstall'),
     url(r'^tongji/$','kx.views.tongji',name='tongji'),
     url(r'^login_tongji/$','kx.views.login_tongji',name='login_tongji'),
