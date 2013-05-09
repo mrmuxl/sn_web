51afc3833a8508e1c07605c9412ace3f9719c682
xulin <mrmuxl@sina.com>
2013-05-09 11:28:17 +0800

    修改配置文件检测环境变量RUN_ENV，根据环境变量加载运行配置文件，如果没有环境变量RUN_ENV或者RUN_ENV为空，默认用dev代替

---
+++ kx/publish/utils.py
@@ -0,0 +1,28 @@
+#_*_coding:utf-8_*_
+
+import logging,os
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
+
+def handle_uploaded_file(ver=None,ins=None,patch=None):
+    path_root = settings.PUBLISH_UPLOAD
+    if ins is not None:
+        path_folder=path_root + "/Install/"
+        path_upload = path_folder + "Qianmo_" + ver + ".zip"
+        do_upload(path_folder,path_upload,ins)
+    if patch is not None:
+        path_folder=path_root + "/Install/" + ver + "/"
+        path_upload = path_folder + "Patch.zip"
+        do_upload(path_folder,path_upload,patch)
+
+def do_upload(path_folder,path_upload,upload_file):
+    try:
+        if not os.path.isdir(path_folder):
+            os.makedirs(path_folder)
+        with open(path_upload,'wb') as fd:
+            for chunk in upload_file.chunks():
+                fd.write(chunk)
+    except IOError as e:
+        logger.debug(u"文件上传失败！%s",e)
+        raise 
