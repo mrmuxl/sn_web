c5df9ccd046e661c390426e12b3fa444c631299d
xulin <mrmuxl@sina.com>
2013-05-19 18:14:15 +0800

    新文件：    requirements.txt

--- doc/mysql_kx/create.sql
+++ doc/mysql_kx/create.sql
@@ -18,6 +18,7 @@ CREATE TABLE `kx_share` (
       `owner_mac` varchar(100) NOT NULL,
       `is_del` tinyint(3) unsigned NOT NULL default '0',
       `owner_kx_user_id` int(10) unsigned default NULL,
+      `share_file_type` tinyint(3) unsigned NOT NULL default '0' COMMENT '0=normal folder,1=normal file',
       PRIMARY KEY  (`id`,`uuid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -56,4 +57,44 @@ CREATE TABLE `kx_share_follow` (
       `follower_kx_user_id` int(10) unsigned default NULL,
       PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `kx_user_groups`
+--
+
+DROP TABLE IF EXISTS `kx_user_groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kx_user_groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kxuser_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `kxuser_id` (`kxuser_id`,`group_id`),
+  KEY `kx_user_groups_1eba34a9` (`kxuser_id`),
+  KEY `kx_user_groups_5f412f9a` (`group_id`),
+  CONSTRAINT `kxuser_id_refs_id_434910cf` FOREIGN KEY (`kxuser_id`) REFERENCES `kx_user` (`id`),
+  CONSTRAINT `group_id_refs_id_4d5845f5` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `kx_user_user_permissions`
+--
+
+DROP TABLE IF EXISTS `kx_user_user_permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kx_user_user_permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kxuser_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `kxuser_id` (`kxuser_id`,`permission_id`),
+  KEY `kx_user_user_permissions_1eba34a9` (`kxuser_id`),
+  KEY `kx_user_user_permissions_83d7f98b` (`permission_id`),
+  CONSTRAINT `kxuser_id_refs_id_1bb8a2db` FOREIGN KEY (`kxuser_id`) REFERENCES `kx_user` (`id`),
+  CONSTRAINT `permission_id_refs_id_087c9a86` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
