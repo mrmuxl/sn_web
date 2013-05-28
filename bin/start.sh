a7ea8b4749203421f78d4b80ae0014ac53e1a08e
xulin <mrmuxl@sina.com>
2013-05-28 15:25:27 +0800

    修改：      start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export RUN_ENV=deploy
 ERROR_LOG_FILE=/var/log/nginx/kx_error.log
-ACCESS_LOG_FILE=/var/log/nginx/kx_access.log
+ACCESS_LOG_FILE=/var/log/nginx/gevent_access.log
 python ../manage.py run_gunicorn -k gevent -w 5 -t 600  --max-requests=1000 --preload --worker-connections=2000 --log-level=debug --error-logfile=${ERROR_LOG_FILE} --access-logfile=${ACCESS_LOG_FILE} --pid=gunicorn.pid -D
