9faa529d30b43a33b0f936d29480f80b8b6f40cc
xulin <mrmuxl@sina.com>
2013-06-06 20:39:39 +0800

    调整目录结构

--- kx/blog/urls.py
+++
@@ -1,9 +0,0 @@
-#_*_coding:utf-8_*_
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-    url(r'^$','kx.blog.views.show',name ='show'),
-    url(r'^show/?$','kx.blog.views.show',name ='show'),
-    url(r'^help/?$','kx.blog.views.help',name ='help'),
-)
-
