b872163262298dd262194652886522092a3c9d49
xulin <mrmuxl@sina.com>
2013-06-07 18:20:56 +0800

    添加 tools/cp60.py 将snap_60X60_开头的缩略图拷贝为不带这个前缀

--- tools/cp60.py
+++ tools/cp60.py
@@ -7,14 +7,14 @@ import os.path
 import sys
 
 def cp60(args,dirname,filename):
-    #print args,dirname,filename
     for i in filename:
-        print i
         try:
             if i.startswith('snap_60X60_'):
                 fn = i.replace('snap_60X60_','')
-                print dirname+'/'+fn
-                #shutil.copy2(dirname+filename,dirname+fn)
+                src = dirname + '/' + i
+                des= dirname+'/'+fn
+                print 'from:%s   to:%s' %(src,des)
+                shutil.copy2(src,des)
         except Exception as e:
             print "error,filename not startswith snap"
             pass
