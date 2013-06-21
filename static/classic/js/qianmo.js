253b7bd9d9db17ce1bb960ed6a9afa6e34da3ea4
xulin <mrmuxl@sina.com>
2013-06-21 10:22:17 +0800

    login页面email提示

--- static/classic/js/qianmo.js
+++ static/classic/js/qianmo.js
@@ -204,3 +204,139 @@ $(function(){
 
         }
     }
+
+$(function(){
+   $("#email").mailAutoTip();
+});
+
+
+//邮件自动完成提示插件
+var mailArr=new Array("@qq.com","@sina.com","@163.com","@126.com","@sohu.com", "@gmail.com","@hotmail.com","@msn.com");
+(function(){
+    $.fn.mailAutoTip = function(options){
+        var setting = {
+            subBox:"#MailAutoTip",/*提示框ID*/
+            subOp:"li",/*options*/
+            id:"#email",/*文本框ID*/
+            mailArr:mailArr,//数组对象
+            hoverClass:"on",/*选中效果*/
+            _cur:0 /*当前的index*/
+        };
+        var opts = $.extend({}, setting, options || {});
+       
+        //tipFun函数
+        var tipFun = function(_v,o){
+            opts._cur=0;
+            var _that=o;
+            $(opts.subBox).show();
+            var str="<ul>";
+            str += "<li id=\"e_type\">&nbsp;请选择邮箱类型:</li>";
+            var e=_v.indexOf("@");
+            if(e==-1){
+                $.each(opts.mailArr,function(s,m){
+                    str+='<li><a href="javascript:void(0)"  >' + _v + m + "</a></li>";                          
+                });
+            }else{
+                var _sh=_v.substring(0,e)
+                var _se=_v.substring(e);
+                var ind = 0;
+                $.each(opts.mailArr, function (s,m) {
+                    if(m.indexOf(_se)!=-1){
+                        str += '<li><a href="javascript:void(0)" >' + _sh + m + "</a></li>";
+                        ind = 1;
+                    }
+                });
+                if(ind==0){
+                    str += '<li><a class="cur_val" href="javascript:void(0)" >' + _v + "</a></li>";
+                }
+            }
+            str+="</ul>";
+            $(opts.subBox).html(str); 
+
+            /*绑定hover事件*/
+            $(opts.subBox).find(opts.subOp).hover(function(){
+                var _that=$(this);
+                _that.addClass(opts.hoverClass);                       
+            },function(){
+                var _that=$(this);
+                _that.removeClass(opts.hoverClass)          
+            });
+            /*绑定click事件*/
+            $(opts.subBox).find(opts.subOp).each(function(){
+                $(this).click(function(e){
+                    if($(e.target).attr("id")!="e_type"){
+                        $(opts.id).val($(e.target).html());
+                        $(opts.subBox).hide();
+                        e.stopPropagation();
+                    }
+                });                                           
+            })
+        };
+
+        /*itemFun*/
+        var itemFun = function(){
+            var _tempArr=$(opts.subBox).find(opts.subOp);
+            var _size=_tempArr.size();
+            for(var i=0;i<_size;i++){
+                _tempArr.eq(i).removeClass(opts.hoverClass);
+            }
+            
+            if(_size>1){
+                if(opts._cur>_size-1){
+                    opts._cur=1;    
+                }
+                if(opts._cur<1){
+                    opts._cur=_size-1;  
+                }
+                _tempArr.eq(opts._cur).addClass(opts.hoverClass);
+            }else{
+                opts._cur=1;    
+            }
+        };
+
+        $(opts.id).keyup(function(e){
+            var _that=$(this);
+            if(_that.val()!=""){
+                if(e.keyCode!=38 && e.keyCode!=40 && e.keyCode!=13 && e.keyCode!=27){
+                    var _inputVal=_that.val();
+                    tipFun(_inputVal,_that);
+                }
+            }else{
+                $(opts.subBox).hide();
+            }
+        });
+
+        /*点击页面其他地方关闭提示层*/
+        $(document).bind("click",function(e){
+            $(opts.subBox).hide();
+        });
+        
+        $(document).keydown(function(e){
+            switch(e.keyCode){
+                case 40://下键
+                    opts._cur++;
+                    itemFun()
+                    break;
+                case 38://上键
+                    opts._cur--;
+                    itemFun()
+                    break;
+                default:
+                   break;
+            }
+        })
+        /*文本框keydown*/
+        $(opts.id).keydown(function(e){
+            var _temp = $(opts.subBox).find(opts.subOp);
+            if(e.keyCode == 13){
+                if(opts._cur!=0){
+                    $(this).val(_temp.eq(opts._cur).text());
+                    opts._cur = 0;
+                }
+                $(opts.subBox).hide();
+                e.stopPropagation();
+            }                             
+        });
+
+    }     
+})(jQuery);
