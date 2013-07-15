06be027cdff1a3947dbef1210c2f76bcc94bde2c
xulin <mrmuxl@sina.com>
2013-07-15 23:13:56 +0800

    新文件：    apps/vipuser/mail_text.py 	修改：      apps/vipuser/urls.py 	修改：      apps/vipuser/views.py 	修改：      qm_web/urls.py 	新文件：    tools/f.txt 	新文件：    tools/send_vip.sh

---
+++ apps/vipuser/mail_text.py
@@ -0,0 +1,64 @@
+#_*_coding:utf-8_*_
+def vipuser_tip(d):
+    msg =u''' <html> <head> 
+    <meta http-equiv="content-type" content="text/html; charset=GB2312">
+  </head>
+  <body bgcolor="#FFFFFF" text="#000000">
+      <meta http-equiv="Content-Type" content="text/html; charset=GB2312">
+      <style>
+BLOCKQUOTE {
+    MARGIN-TOP: 0px; MARGIN-BOTTOM: 0px; MARGIN-LEFT: 2em
+}
+OL {
+    MARGIN-TOP: 0px; MARGIN-BOTTOM: 0px
+}
+UL {
+    MARGIN-TOP: 0px; MARGIN-BOTTOM: 0px
+}
+P {
+    MARGIN-TOP: 0px; MARGIN-BOTTOM: 0px
+}
+BODY {
+    FONT-SIZE: 10.5pt; COLOR: #000000; LINE-HEIGHT: 1.5; FONT-FAMILY: 宋体
+}
+</style>
+      <meta content="MSHTML 6.00.2900.6400" name="GENERATOR">
+      <div>你好，</div>
+      <div style="TEXT-INDENT: 2em">SimpleNect新推出了软件仓库功能，欢迎下载试用，地址：</div>
+      <div style="TEXT-INDENT: 2em">&nbsp;</div>
+      <div style="TEXT-INDENT: 2em"><a moz-do-not-send="true"
+          href="'''
+    msg += d 
+    msg +=u'''">'''
+    msg += d
+    msg += u'''</a></div>
+      <div style="TEXT-INDENT: 2em">&nbsp;</div>
+      <div style="TEXT-INDENT: 2em">软件仓库是配合SimpleNect客户端使用的一个独立软件，你可以用它来
+        构建一个简单的文件服务器。</div>
+      <div style="TEXT-INDENT: 2em">步骤：</div>
+      <div style="TEXT-INDENT: 2em">1 安装软件仓库到一台服务器上</div>
+      <div style="TEXT-INDENT: 2em">2 授权用户访问软件仓库</div>
+      <div style="TEXT-INDENT: 2em">&nbsp;</div>
+      <div style="TEXT-INDENT: 2em">设置好仓库后你可以登录SimpleNect客户端使用仓库了，你可以这么
+        用：</div>
+      <div style="FONT-WEIGHT: bold; TEXT-INDENT: 2em">1 备份文件</div>
+      <div style="TEXT-INDENT: 4em">指定一个目录后，SimpleNect客户端会自动同步文件到仓库。</div>
+      <div style="FONT-WEIGHT: bold; TEXT-INDENT: 2em">2 离线共享</div>
+      <div style="TEXT-INDENT: 4em">&nbsp;同步到仓库的目录共享给好友后，即使你离线了好友仍然可以看到。</div>
+      <div style="TEXT-INDENT: 2em">3 更多功能等待你的发现</div>
+      <div style="TEXT-INDENT: 2em">&nbsp;</div>
+      <div style="TEXT-INDENT: 2em">&nbsp;</div>
+      <div style="TEXT-INDENT: 2em">欢迎把你的使用体验反馈给我们，我们的QQ用户群：205528924</div>
+      <div style="TEXT-INDENT: 2em">也可以访问我们的网站：<a moz-do-not-send="true"
+          href="http://www.simplenect.cn">http://www.simplenect.cn</a></div>
+      <div style="TEXT-INDENT: 2em">&nbsp;</div>
+      <div style="TEXT-INDENT: 26em">---- SimpleNect开发团队</div>
+      <br>
+    </div>
+    <br>
+  </body>
+</html>
+'''
+    return msg
+
+
