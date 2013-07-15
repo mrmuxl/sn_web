714ce19bf592ba4130fc4b4e33ad433aad47043a
xulin <mrmuxl@sina.com>
2013-07-15 23:21:01 +0800

    修改：      send_vip.sh

--- tools/send_vip.sh
+++ tools/send_vip.sh
@@ -2,6 +2,6 @@
 for i in `cat f.txt`
 do
     echo $i
-    curl http://www.simeplenect.cn/vipuser/vipuser_test/518279d14e20e/$i
+    curl http://www.simplenect.cn/vipuser/vipuser_test/518279d14e20e/$i
     sleep 2
 done
