46ccfd8fb9cab95a2cc310e943b231c4cd19f65b
xulin <mrmuxl@sina.com>
2013-09-16 18:41:47 +0800

    修改：      apps/spool/models.py 	修改：      apps/spool/urls.py 	修改：      apps/spool/views.py 	修改：      templates/classic/buy.html 	修改：      tools/order_process.py

--- apps/spool/urls.py
+++ apps/spool/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^add/?$','apps.spool.views.spool_add',name ='spool_add'),
+    url(r'^select/?$','apps.spool.views.spool_select',name ='spool_select'),
+    url(r'^update/?$','apps.spool.views.spool_upate',name ='spool_update'),
 )
 
