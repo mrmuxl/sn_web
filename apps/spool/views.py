50b2ccfd5a4599bec0d755c985bcc2792255f662
xulin <mrmuxl@sina.com>
2013-09-17 17:54:05 +0800

    修改：      apps/spool/models.py 	修改：      apps/spool/views.py

--- apps/spool/views.py
+++ apps/spool/views.py
@@ -10,6 +10,7 @@ from django.shortcuts import render
 from forms import SpoolForm
 from models import Spool 
 from django.conf import settings
+from pprint import pprint
 
 logger = logging.getLogger(__name__)
 
@@ -19,38 +20,52 @@ def spool_add(request):
     message = {}
     form = SpoolForm(request.POST)
     if form.is_valid():
-        try:
-            form.save()
-        except Exception as e:
-            logger.debug("spool add=>%s",e)
+        form.save()
         message['status']=0
         return HttpResponse(json.dumps(message),content_type="application/json")
     else:
         message['status']=1
         return HttpResponse(json.dumps(message),content_type="application/json")
 
+@csrf_exempt
 @require_POST
 def spool_select(request):
+    message = {}
     email = request.POST.get('email','')
-    uuid  = request.POST.get('uuid','')
-    if email and uuid:
-        message['status']="ok"
+    mac  = request.POST.get('mac','')
+    if email and mac:
+        origin_list = Spool.objects.filter(origin_email=email).filter(origin_uuid=mac).values()
+        accept_list = Spool.objects.filter(accept_email=email).filter(accept_uuid=mac).values()
+        print origin_list,accept_list
+        if origin_list:
+            message['origin'] = str(origin_list)
+        else:
+            message['origin'] = []
+        if accept_list:
+            message['accept'] = str(accept_list)
+        else:
+            message['accept'] = []
+        message['status']= 0
         return HttpResponse(json.dumps(message),content_type="application/json")
     else:
-        message['status']="error"
+        message['status']= 1
         return HttpResponse(json.dumps(message),content_type="application/json")
 
-
-    
+@csrf_exempt
 @require_POST
 def spool_update(request):
     uuid  = request.POST.get('uuid','')
     status = request.POST.get('status','')
     if uuid and status:
-        message['status']="ok"
-        return HttpResponse(json.dumps(message),content_type="application/json")
+        try:
+            Spool.objects.filter(uuid=uuid).update(status=status)
+            message['status'] = 0
+            return HttpResponse(json.dumps(message),content_type="application/json")
+        except Exception as e:
+            message['status'] = 1
+            return HttpResponse(json.dumps(message),content_type="application/json")
     else:
-        message['status']="error"
+        message['status'] = 1
         return HttpResponse(json.dumps(message),content_type="application/json")
 
 
