a881bb761fd05c8cfd419c290271381e38dcbcf1
xulin <mrmuxl@sina.com>
2013-09-18 13:42:18 +0800

    修改：      apps/alipay/templates/alipay/order_info.html 	修改：      apps/spool/views.py

--- apps/spool/views.py
+++ apps/spool/views.py
@@ -21,6 +21,7 @@ logger = logging.getLogger(__name__)
 def spool_add(request):
     message = {}
     form = SpoolForm(request.POST)
+    print request.POST
     if form.is_valid():
         form.save()
         message['status']=0
