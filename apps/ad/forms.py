a8b384fe99fc43604959533f1faf7d62252cbc88
xulin <mrmuxl@sina.com>
2013-09-19 01:36:59 +0800

    修改：      apps/ad/forms.py 	修改：      apps/ad/models.py 	修改：      apps/ad/views.py 	修改：      apps/kx/views.py 	修改：      apps/spool/views.py 	修改：      qm_web/urls.py 	重命名：    templates/classic/print.html -> templates/classic/ad/print.html 	新文件：    templates/classic/ad/print_result.html

--- apps/ad/forms.py
+++ apps/ad/forms.py
@@ -1,10 +1,14 @@
 #coding=utf-8
 from django import forms
 from django.utils.translation import ugettext_lazy as _
-from models import KxSoftAd
+from models import KxSoftAd,PrinterPop
 
 
 class AdForm(forms.ModelForm):
     class Meta:
         adform = KxSoftAd
 
+
+class PrinterPopForm(forms.ModelForm):
+    class Meta:
+        ppform = PrinterPop
--- apps/ad/views.py
+++ apps/ad/views.py
@@ -1,7 +1,9 @@
 #_*_coding:utf-8_*_
 
 import datetime,logging,json,os
-from models import KxSoftAd
+from models import KxSoftAd,PrinterPop
+from forms import PrinterPopForm
+from apps.publish.models import KxPub
 from django.views.decorators.http import (require_POST,require_GET)
 from django.http import HttpResponseRedirect,HttpResponse
 from django.shortcuts import render
@@ -42,3 +44,31 @@ def ad_api(request):
         message['status']="0"
         message['desc']= 'error'
         return HttpResponse(json.dumps(message),content_type="application/json")
+
+def printer(request):
+    if request.method == 'GET':
+        data={"title":u"打印创业计划"}
+        try:
+            ins_file = KxPub.objects.filter(pub_time__isnull=False).order_by('-id')[0:1].get()
+        except Exception as e:
+            data.update(ins_file='')
+            logger.debug("ins_file:%s",e)
+        data.update(ins_file=ins_file.install_file)
+        return render(request,"ad/print.html",data)
+    if request.method == 'POST':
+        data={"title":u"打印创业计划"}
+        email = request.POST.get('pop','')
+        if email:
+            try:
+                p = PrinterPop.objects.create(email=email)
+                p.save()
+            except Exception as e:
+                logger.debug("printer view:%s",e)
+        try:
+            ins_file = KxPub.objects.filter(pub_time__isnull=False).order_by('-id')[0:1].get()
+        except Exception as e:
+            data.update(ins_file='')
+            logger.debug("ins_file:%s",e)
+        data.update(ins_file=ins_file.install_file)
+        return render(request,"ad/print_result.html",data)
+
