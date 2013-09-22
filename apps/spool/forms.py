10915752400711b9cb60d14e6bca442df72d0af5
xulin <mrmuxl@sina.com>
2013-09-22 16:51:25 +0800

    修改：      apps/spool/forms.py

--- apps/spool/forms.py
+++ apps/spool/forms.py
@@ -5,6 +5,6 @@ from models import Spool
 class SpoolForm(forms.ModelForm):
     class Meta:
         model = Spool
-        fields = ['uuid','origin_email','accept_email','origin_uuid','accept_uuid','printer_name','printer_uuid','file_name','file_path','page_num']
+        fields = ['uuid','origin_email','accept_email','origin_uuid','accept_uuid','printer_name','printer_uuid','file_name','file_path','file_client_path','page_num']
 
 
