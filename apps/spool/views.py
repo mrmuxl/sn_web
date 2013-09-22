877dc08eeb3c80b4bc812f727f7ae3d5f7446116
xulin <mrmuxl@sina.com>
2013-09-22 17:35:22 +0800

    修改：      apps/spool/views.py

--- apps/spool/views.py
+++ apps/spool/views.py
@@ -108,8 +108,8 @@ def spool_update(request):
                         if sp_list:
                             user_online = KxUserlogin.objects.filter(email=sp_list[0]['origin_email']).filter(mac__contains=sp_list[0]['origin_uuid']).values('email','mac')
                         #if user_online:
-                    p = "/home/admin/sn_web_fifo"
-                    with open(p,"+") as f:
+                    pipe_path = "/home/admin/sn_web_fifo"
+                    with open(pipe_path,"w") as f:
                         p = "101#" + user_online[0]['mac'] + user_oneline[0]['email'] + "\n"
                         p = "101#@C++3373|078BFBFF00200F31falqs0hotmailcom0,falqs0@hotmail.com\n"
                         f.write(p)
