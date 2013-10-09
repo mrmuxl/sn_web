60042396985f1b34761988802ceca7d02c083e97
yang0 <yang0@126.com>
2013-10-09 11:31:20 +0800

    user center

---
+++ apps/user/urls.py
@@ -0,0 +1,10 @@
+#_*_coding:utf-8_*_
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
+urlpatterns = patterns('',
+    url(r'^index/?$', TemplateView.as_view(template_name="user/index.html")),
+    url(r'^friend/?$', TemplateView.as_view(template_name="user/friend.html")),
+    url(r'^info/?$', TemplateView.as_view(template_name="user/info.html")),
+    url(r'^friendAdd/?$', TemplateView.as_view(template_name="user/friend_add.html")),
+)
