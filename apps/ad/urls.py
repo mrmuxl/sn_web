00b5388ad8052fcd667da1d21baf693dfe8f6a2c
yang0 <yang0@126.com>
2013-10-09 15:11:56 +0800

    创业加盟-运营商申请页面

--- apps/ad/urls.py
+++ apps/ad/urls.py
@@ -1,9 +1,11 @@
 #_*_coding:utf-8_*_
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$','apps.ad.views.ad_list',name ='ad_list'),
     url(r'ad_list/?$','apps.ad.views.ad_list',name ='ad_list'),
     url(r'getAdList/?$','apps.ad.views.ad_api'),
+    url(r'operator/add/?$',TemplateView.as_view(template_name="ad/operator_add_form.html")),
 )
 
