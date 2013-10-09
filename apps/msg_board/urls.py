acd44e2dca7e9749d24b3f1ff901115982583233
xulin <mrmuxl@sina.com>
2013-10-10 03:05:23 +0800

    修改：      apps/blog/admin.py 	修改：      apps/msg_board/urls.py 	修改：      config/dev.py 	修改：      templates/classic/admin_header.html 	修改：      templates/classic/header.html 	修改：      templates/classic/index.html

--- apps/msg_board/urls.py
+++ apps/msg_board/urls.py
@@ -1,11 +1,22 @@
 #_*_coding:utf-8_*_
 from django.conf.urls import patterns, include, url
+from django.views.generic import DetailView, ListView
+
+#urlpatterns = patterns('',
+#    url(r'^$','apps.msg_board.views.msg_board',name ='msg_index'),
+#    url(r'^msg_index/?$','apps.msg_board.views.msg_board',name ='msg_index'),
+#    url(r'^add_msg/?$','apps.msg_board.views.add_msg',name ='add_msg'),
+#    url(r'^del_msg/?$','apps.msg_board.views.del_msg',name ='del_msg'),
+#    url(r'^check_msg/?$','apps.msg_board.views.check_msg',name ='check_msg'),
+#)
+
 
 urlpatterns = patterns('',
-    url(r'^$','apps.msg_board.views.msg_board',name ='msg_index'),
-    url(r'^msg_index/?$','apps.msg_board.views.msg_board',name ='msg_index'),
-    url(r'^add_msg/?$','apps.msg_board.views.add_msg',name ='add_msg'),
-    url(r'^del_msg/?$','apps.msg_board.views.del_msg',name ='del_msg'),
-    url(r'^check_msg/?$','apps.msg_board.views.check_msg',name ='check_msg'),
+    url(r'^$','apps.forum.views.index',name ='forum_index'),
+    url(r'^index/?$','apps.forum.views.index',name ='forum_index'),
+    url(r'^add/?$','apps.forum.views.add',name ='forum_add'),
+    url(r'^post/(?P<pid>\d+)/?$', 'apps.forum.views.post',name='forum_post'),
+    url(r'^reply/?$','apps.forum.views.reply',name ='forum_reply'),
+    url(r'^vote/?$','apps.forum.views.vote',name ='forum_vote'),
 )
 
