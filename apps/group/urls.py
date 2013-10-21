a679368cf2a69c4fa7d8c4ef50e992933ea607aa
mokeming <mokeming@163.com>
2013-10-21 18:45:43 +0800

    群功能模块

---
+++ apps/group/urls.py
@@ -0,0 +1,9 @@
+#_*_coding:utf-8_*_
+
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
+urlpatterns = patterns('',
+    # url(r'^$','apps.group.views.login',name='login'),
+    url(r'^user_remark/?$','apps.group.views.user_remark', name='user_remark'),
+)
