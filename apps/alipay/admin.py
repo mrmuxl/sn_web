be8b1aa2e308acb5355dc1e0a1838062b5e1f950
xulin <mrmuxl@sina.com>
2013-10-22 09:43:15 +0800

    修改：      apps/accounts/views.py 	修改：      apps/alipay/admin.py 	修改：      apps/alipay/urls.py 	修改：      apps/alipay/views.py 	修改：      apps/group/admin.py 	修改：      apps/group/models.py

--- apps/alipay/admin.py
+++ apps/alipay/admin.py
@@ -7,7 +7,7 @@ class ProductInfoAdmin(admin.ModelAdmin):
     list_display = ('name','category','price','stocked')
 
 class OrderInfoAdmin(admin.ModelAdmin):
-    list_filter = ('pay_status',)
+    list_filter = ('pay_status','status')
     list_display = ('order_id','buy_user','total_fee','create_at','buy_product','number','pay_status','pay_at','trade_no','status','auth_user_num')
 
 admin.site.register(ProductInfo,ProductInfoAdmin)
