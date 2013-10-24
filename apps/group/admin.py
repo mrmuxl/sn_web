6c0c513100449d71db69134c427d52940bb46b81
xulin <mrmuxl@sina.com>
2013-10-24 14:28:00 +0800

    groups admin 	修改：      apps/accounts/urls.py 	修改：      apps/group/admin.py 	修改：      apps/group/models.py 	新文件：    templates/classic/group/show_group_users.html

--- apps/group/admin.py
+++ apps/group/admin.py
@@ -2,14 +2,36 @@
 
 from django.contrib import admin
 from django.conf import settings
+from django.db.models import Max
 from models import Groups,GroupUser,GroupUserVerify,GroupPrintAuth
 from models import GroupPrint,GroupManager
 
+
 class GroupsAdmin(admin.ModelAdmin):
-    pass
+    list_display =('id','name','owner_id','user_num','max_num','g_type','create_time','creater_id')
+    def save_model(self,request,obj,forum,change):
+        obj.creater_id =request.user
+        if obj.g_type == 1:#普通群
+            r = Groups.objects.filter(id__gte=20000).aggregate(max_id=Max('id'))
+            if r['max_id'] == None:
+                obj.pk = 20000
+            else:
+               obj.pk = r['max_id'] +1 
+        elif obj.g_type == 2:#高校群
+            r = Groups.objects.filter(id__gte=10001).filter(id__lte=19999).aggregate(max_id=Max('id'))
+            if r['max_id'] == None:
+                obj.pk = 10001
+            else:
+               obj.pk = r['max_id'] +1 
+        obj.save()
+
 
 class GroupUserAdmin(admin.ModelAdmin):
-    pass
+    list_display =('user_id','user_remark','share_print','join_time','joiner_id')
+    def save_model(self,request,obj,forum,change):
+        obj.joiner_id =request.user
+        obj.save()
+   
 
 class GroupUserVerifyAdmin(admin.ModelAdmin):
     pass
@@ -26,7 +48,7 @@ class GroupManagerAdmin(admin.ModelAdmin):
 if settings.DEBUG:
     admin.site.register(Groups,GroupsAdmin)
     admin.site.register(GroupUser,GroupUserAdmin)
-    admin.site.register(GroupUserVerify,GroupUserVerifyAdmin)
-    admin.site.register(GroupPrintAuth,GroupPrintAuthAdmin)
-    admin.site.register(GroupPrint,GroupPrintAdmin)
-    admin.site.register(GroupManager,GroupManagerAdmin)
+#    admin.site.register(GroupUserVerify,GroupUserVerifyAdmin)
+#    admin.site.register(GroupPrintAuth,GroupPrintAuthAdmin)
+#    admin.site.register(GroupPrint,GroupPrintAdmin)
+#    admin.site.register(GroupManager,GroupManagerAdmin)
--- apps/group/models.py
+++ apps/group/models.py
@@ -12,43 +12,49 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin)
 from django.conf import settings
+from datetime import datetime
+from apps.kx.models import KxUser
 
 
 # 群
 class Groups(models.Model):
     id = models.AutoField(primary_key=True)  # 高校群ID 10001~19999 普通群 >=20000
-    name = models.CharField(max_length=30)
-    owner_id = models.CharField(max_length=32)
-    user_num = models.IntegerField()
-    max_num = models.IntegerField(verbose_name=_(u'群最打用户数'))
-    g_type = models.IntegerField(default=1,help_text=_(u'1=普通群，2=高校群'))  # 1=普通群 2=高校群
-    create_time = models.DateTimeField()
-    creater_id = models.CharField(max_length=32)
+    name = models.CharField(max_length=30,verbose_name=_(u'群名称'))
+    owner_id = models.OneToOneField(KxUser,verbose_name=_(u'群所有者Email'),related_name='owner_in')
+    user_num = models.IntegerField(default=0,verbose_name=_(u'群用户人数'),editable=False)
+    max_num = models.IntegerField(verbose_name=_(u'群最大用户数'))
+    g_type = models.IntegerField(verbose_name=_(u'群类型'),default=1,help_text=_(u'1=普通群，2=高校群'))  # 1=普通群 2=高校群
+    create_time = models.DateTimeField(default=datetime.now,verbose_name=_(u'创建时间'))
+    creater_id = models.OneToOneField(KxUser,verbose_name=_(u'创建群的人'),editable=False,related_name='create_id')# 谁被加进来的
 
     class Meta:
         db_table = 'groups'
         verbose_name_plural = verbose_name = _(u'群')
+    def __unicode__(self):
+        return self.name
 
 
 # 群用户
 class GroupUser(models.Model):
     id = models.AutoField(primary_key=True)
-    group_id = models.IntegerField()
-    user_id = models.CharField(max_length=32)
-    user_remark = models.CharField(max_length=20,null=True)  # 用户备注名
-    share_print = models.BooleanField()  # 是否可以共享打印机到群 0=否
-    join_time = models.DateTimeField()
-    joiner_id = models.CharField(max_length=32)
+    group_id = models.ManyToManyField(Groups,verbose_name=_(u'群ID'),related_name='group_id')
+    user_id = models.OneToOneField(KxUser,verbose_name=_(u'群用户Email'),related_name='user_id')# 谁被加进来的
+    user_remark = models.CharField(max_length=20,null=True,blank=True,verbose_name=_(u'用户备注名'))  # 用户备注名
+    share_print = models.BooleanField(verbose_name=_(u'是否可以共享打印机到群'),help_text=_(u'0:不能添加共享打印机到群'))  # 是否可以共享打印机到群 0=否
+    join_time = models.DateTimeField(default=datetime.now,verbose_name=_(u'加入群的时间'))
+    joiner_id = models.OneToOneField(KxUser,verbose_name=_(u'加群的人的Email'),editable=False,related_name='joiner_id')# 谁被加进来的
 
     class Meta:
         db_table = "group_user"
         verbose_name_plural = verbose_name = _(u'群用户')
+    #def __unicode__(self):
+    #    return self.user_id_id
 
 
 # 申请加群记录
 class GroupUserVerify(models.Model):
     id = models.AutoField(primary_key=True)
-    group_id = models.IntegerField()
+    group_id = models.IntegerField(verbose_name=_(u'群ID'))
     user_id = models.IntegerField(verbose_name=_(u'申请用户ID'))  # 申请用户ID
     msg = models.CharField(max_length=50,verbose_name=_(u'申请用户留言'))  # 申请用户的留言
     create_time = models.DateTimeField()
@@ -61,12 +67,12 @@ class GroupUserVerify(models.Model):
 # 群用户打印权限
 class GroupPrintAuth(models.Model):
     id = models.AutoField(primary_key=True)
-    group_id = models.IntegerField()
-    print_user_id = models.CharField(max_length=32)  # 共享打印机的用户ID
-    user_id = models.CharField(max_length=32)  # 使用者用户ID
+    group_id = models.IntegerField(verbose_name=_(u'群ID'))
+    print_user_id = models.CharField(max_length=32,verbose_name=_(u'共享打印机的用户ID'))  # 共享打印机的用户ID
+    user_id = models.CharField(max_length=32,verbose_name=_(u'使用这用户ID'))  # 使用者用户ID
     status = models.IntegerField(verbose_name=_(u'审核状态'),help_text=_(u'0=审核中，1=审核通过，2=拒绝'))  # 审核状态 0=审核中，1=审核通过，2=拒绝
-    answer = models.CharField(null=True,max_length=30)
-    create_time = models.DateTimeField()
+    answer = models.CharField(null=True,max_length=30,verbose_name=_(u'提问回答'))
+    create_time = models.DateTimeField(default=datetime.now,verbose_name=_(u'创建时间'))
 
     class Meta:
         db_table = "group_print_auth"
@@ -77,7 +83,7 @@ class GroupPrintAuth(models.Model):
 class GroupPrint(models.Model):
     id = models.AutoField(primary_key=True)
     group_id = models.IntegerField()
-    print_user_id = models.CharField(max_length=32)
+    print_user_id = models.CharField(max_length=32,verbose_name=_(u'打印机用户ID'))
     print_name = models.CharField(max_length=30,verbose_name=_(u'打印机名称'))  # 打印机名称
     print_code = models.CharField(max_length=260,verbose_name=_(u'打印机序列号'))  # 打印机序列号
     create_time = models.DateTimeField()
@@ -90,8 +96,8 @@ class GroupPrint(models.Model):
 # 群管理员
 class GroupManager(models.Model):
     id = models.AutoField(primary_key=True)
-    group_id = models.IntegerField()
-    user_id = models.CharField(max_length=32)
+    group_id = models.IntegerField(verbose_name=_(u'群ID'))
+    user_id = models.CharField(max_length=32,verbose_name=_(u'管理员Email'))
 
     class Meta:
         db_table = "group_manager"
