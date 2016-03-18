5e668592ecaf5321d6d1d00c4a572aa4f1e38b46
xulin <mrmuxl@sina.com>
2016-03-18 20:16:26 +0800

    升级jquery到1.7.2, csrf还没有搞定

--- kx/admin.py
+++ kx/admin.py
@@ -1,57 +1,13 @@
 #_*_coding:utf-8_*_
 
-from django import forms
+#from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
+#from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 from kx.models import KxUser
-
-
-class UserCreationForm(forms.ModelForm):
-    """A form for creating new users. Includes all the required
-    fields, plus a repeated password."""
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
-
-    class Meta:
-        model = KxUser
-        fields = ('email', 'nick')
-
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-        # Save the provided password in hashed format
-        user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
-
-class UserChangeForm(forms.ModelForm):
-    """A form for updating users. Includes all the fields on
-    the user, but replaces the password field with admin's
-    password hash display field.
-    """
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = KxUser
-
-    def clean_password(self):
-        # Regardless of what the user provides, return the initial value.
-        # This is done here, rather than on the field, because the
-        # field does not have access to the initial value
-        return self.initial["password"]
-
+from kx.admin_forms import (UserChangeForm,UserCreationForm)
 
 class KxUserAdmin(UserAdmin):
     # The forms to add and change user instances
@@ -61,18 +17,17 @@ class KxUserAdmin(UserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email','nick','is_admin')
-    list_filter = ('is_admin',)
+    list_display = ('email','username')
+    list_filter = ('is_staff','is_superuser')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('nick',)}),
-        ('Permissions', {'fields': ('is_admin',)}),
+        ('Personal info', {'fields': ('username',)}),
         ('Important dates', {'fields': ('last_login',)}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'nick', 'password1', 'password2')}
+            'fields': ('email', 'username', 'password1', 'password2')}
         ),
     )
     search_fields = ('email',)
--- kx/views.py
+++ kx/views.py
@@ -3,9 +3,19 @@
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 import datetime
 from django.contrib.auth import get_user_model
+from kx.forms import RegisterForm
+from kx.models import KxUser
+from django.contrib.auth import authenticate
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
+from django.utils import timezone
+import json
+
 
 def index(request):
 
@@ -72,14 +82,66 @@ def msg_board(request):
 
 def  add_msg(request):
     pass
-
 def register(request):
+    '''注册视图'''
+#    now = timezone.now()
+    template_var={}
+#    form = RegisterForm()    
+#    if request.method=="POST":
+#        form=RegisterForm(request.POST.copy())
+#        if form.is_valid():
+#            username=form.cleaned_data["username"]
+#            email=form.cleaned_data["email"]
+#            password=form.cleaned_data["password"]
+#            user=KxUser.objects.create_user(username=username,email=email,password=password,create_time=now,update_time=now)
+#            user.save()
+#            _login(request,username,password)#注册完毕 直接登陆
+#            return HttpResponseRedirect(reverse("index"))    
+#    template_var["form"]=form        
+    #print request
+    return render_to_response("register.html",template_var,context_instance=RequestContext(request))
+    if request.method =="POST":
+        username=request.POST["nick"]
+        email=request.POST["nick"]
+        
+
+def check(request):
+    print request
+    if request.method =="POST":
+        email = request.POST.get("email")
+        email = email.strip() 
+        print email
+        if not email:
+            return HttpResponse(json.dumps({"data":0,"info":"","status":0}),content_type="application/json")
+        else:
+            count = KxUser.objects.filter(email=email).count()
+            print count
+            if count == 0:
+                return HttpResponse(json.dumps({"data":1,"info":"OK","status":1}),content_type="application/json")
+    return HttpResponse(json.dumps({"data":0,"info":"","status":0}),content_type="application/json")
+
+ 
+def save(request):
     pass
 
-def login(request):
-    pass
-def logout(request):
-    pass
 
-def info(register):
-    pass
+#def login(request):
+#    pass
+#def logout(request):
+#    pass
+#
+#def info(request):
+#    pass
+#def _login(request,username,password):
+#    '''登陆核心方法'''
+#    ret=False
+#    user=authenticate(username=username,password=password)
+#    if user:
+#        if user.is_active:
+#            auth_login(request,user)
+#            ret=True
+#        else:
+#            messages.add_message(request, messages.INFO, _(u'用户没有激活'))
+#    else:
+#        messages.add_message(request, messages.INFO, _(u'用户不存在'))
+#    return ret
