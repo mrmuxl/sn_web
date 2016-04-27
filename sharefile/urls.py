364ade125dfa3e1df7a23928063692ffa3898fcb
yang0 <yang0@126.com>
2016-04-27 16:46:58 +0800

    发送自己的信息到服务器

--- sharefile/urls.py
+++ sharefile/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'sharefile.views.friendFiles', name='friendFiles'),
+    url(r'sendMsg2cscServer/$', 'sharefile.views.sendMyInfoToServer', name='sendMsg2cscServer'),
     url(r'^peerPort/$', 'sharefile.views.peerPort', name='peerPort'),
 )
