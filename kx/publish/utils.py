8126839c1594dd4c6142f2db9305202e098f5ec6
xulin <mrmuxl@sina.com>
2016-05-10 10:42:16 +0800

    修改：      .gitignore 	修改：      kx/publish/forms.py 	修改：      kx/publish/urls.py 	修改：      kx/publish/utils.py 	修改：      kx/publish/views.py 	修改：      kx/templates/default/index_download.html 	修改：      kx/templates/default/publish/add.html 	删除：      kx/templates/default/publish/edit.html 	修改：      kx/templates/default/publish/index.html

--- kx/publish/utils.py
+++ kx/publish/utils.py
@@ -23,6 +23,23 @@ def do_upload(path_folder,path_upload,upload_file):
         with open(path_upload,'wb') as fd:
             for chunk in upload_file.chunks():
                 fd.write(chunk)
+    except OSError as e:
+        logger.debug(u"目录创建失败！%s",e)
+        raise 
     except IOError as e:
         logger.debug(u"文件上传失败！%s",e)
+        raise 
+
+def update_download_link(install_file):
+    print install_file
+    download_html = u'<span> <a href="' + settings.DOMAIN + u'/Install/' + install_file + u'" target="_blank" title="下载">'
+    download_html += u'<img src="{{ STATIC_URL }}images/download_btn2.png" class="mypng" /></a></span>'
+    download_html=download_html.encode('utf8')
+    fname = os.path.join(settings.ROOT_DIR,'templates' + settings.THEME)
+    fname += "index_download.html"
+    try:
+        with open(fname,'w') as fd:
+            fd.write(download_html)
+    except EnvironmentError as e:
+        logger.debug(u"写跟新发布文件错误！%s",e)
         raise 
