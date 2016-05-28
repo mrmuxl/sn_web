37d5f0fa397c655176a8d5a2394ce96e60619912
xulin <mrmuxl@sina.com>
2016-05-28 15:10:00 +0800

    修改：      bin/start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export RUN_ENV=deploy
-python ../manage.py run_gunicorn -k gevent -w 10 -t 600  --max-requests=1000 --preload --worker-connections=2000 --log-level=debug --pid=gunicorn.pid -D
+LOG_FILE=/var/log/nginx/kx.log
+python ../manage.py run_gunicorn -k gevent -w 10 -t 600  --max-requests=1000 --preload --worker-connections=2000 --log-level=debug --error-logfile=${LOG_FILE} --pid=gunicorn.pid -D
