93576693ca879122fd7a713b0070257e2393f2c8
yang0 <yang0@126.com>
2016-06-07 16:01:04 +0800

    修改目录结构

--- online_user/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-    url(r'^$', 'online_user.views.onlineFriends', name='onlineFriends'),
-)
--- sharefile/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-    url(r'^$', 'sharefile.views.friendFiles', name='friendFiles'),
-    url(r'sendMsg2cscServer/$', 'sharefile.views.sendMyInfoToServer', name='sendMsg2cscServer'),
-    url(r'^peerPort/$', 'sharefile.views.peerPort', name='peerPort'),
-)
