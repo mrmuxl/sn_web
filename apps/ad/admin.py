5d5ce9a635277bcee16d3f3ebbb370bdd7bfdb28
xulin <mrmuxl@sina.com>
2016-06-07 11:00:56 +0800

    修改：      apps/ad/admin.py 	修改：      apps/ad/models.py 	修改：      apps/ad/urls.py 	修改：      apps/ad/views.py

--- apps/ad/admin.py
+++ apps/ad/admin.py
@@ -3,12 +3,18 @@
 from django.contrib import admin
 from models import KxSoftAd
 from forms import AdForm
+import datetime
 
 class AdAdmin(admin.ModelAdmin):
+    now = datetime.datetime.now()
     ad = AdForm
     list_display =('title','ad_url','exp_day','create_time')
+    fieldsets = (
+        (None,{'fields':('title','ad_url','exp_day')}),
+    )
     def save_model(self,request,obj,ad,change):
+        obj.create_time = self.now
         obj.creater_id =request.user.uuid
-        return super(BlogAdmin, self).save_model(request, obj,ad, change)
+        return super(AdAdmin, self).save_model(request, obj,ad, change)
 
 admin.site.register(KxSoftAd,AdAdmin)
