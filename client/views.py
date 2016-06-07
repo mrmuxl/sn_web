55def0db87faaaa97e3dbe79d388b03ae496e1a4
yang0 <yang0@126.com>
2016-06-07 15:44:37 +0800

    implement snlogin_required

--- client/views.py
+++ client/views.py
@@ -1,1 +1,26 @@
-# Create your views here.
+#_*_coding:utf-8_*_
+import logging
+from django.contrib.auth import authenticate
+from django.contrib import auth
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.html import strip_tags
+from client.decorators import sn_required
+
+logger = logging.getLogger(__name__)
+
+@csrf_exempt
+@sn_required
+def login(request):
+    '''登陆视图'''
+    if request.method == "POST":
+        email = strip_tags(request.POST.get("email",'').lower().strip())
+        password = request.POST.get("password").strip()
+        user = authenticate(username=email,password=password)
+        if user and user.is_active:
+            auth.login(request,user)
+            return HttpResponse("1")
+        else:
+            return HttpResponse("-1")
+    else:
+    	return HttpResponse("-2")
