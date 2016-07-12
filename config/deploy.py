0bdc93091449b2310d70e92b0e82872579b5173b
xulin <mrmuxl@sina.com>
2016-07-12 21:59:13 +0800

    update deploy.py

--- config/deploy.py
+++ config/deploy.py
@@ -131,7 +131,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'gunicorn',
     'apps.kx',
-    #'apps.blog',
+    'apps.blog',
     'apps.sharefile',
     'apps.online_user',
     'apps.auth',
