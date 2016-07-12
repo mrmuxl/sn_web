84b5646d5f814dcbab5aa609ab36231434a61bc2
xulin <mrmuxl@sina.com>
2016-07-12 16:09:38 +0800

    update login views

--- apps/alipay/views.py
+++ apps/alipay/views.py
@@ -1,15 +1,15 @@
 #_*_coding:utf-8_*_
 
-import logging
+import logging,uuid,time
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import (require_POST,require_GET)
 from django.http import HttpResponseRedirect,HttpResponse
 from django.shortcuts import render
+from models import OrderInfo
+from lib.alipay import create_direct_pay_by_user
 
 
 logger = logging.getLogger(__name__)
-
-def index(request):
-    return render(request,"alipay/index.html",{})
 
 def return_url_handler(request):
     pass
@@ -23,7 +23,25 @@ def success(request):
 def error(request):
     pass
 
+@require_GET
+@login_required
+def order_info(request):
+    return render(request,"alipay/order_result.html",{})
+
 @require_POST
 def order_result(request):
-    t = request.POST.get('order_type','')
-    return render(request,"alipay/order_result.html",{})
+    order_id = time.strftime("%Y%m%d") + str(uuid.UUID.get_time_low(uuid.uuid1()))
+    print order_id
+    t = request.POST.get('type','12')
+    if t == 12:
+        name = u'SimpleNectVIP'
+        desc = u'12个月'
+        price = 1000 
+    elif t == 6:
+        name = u'SimpleNectVIP'
+        desc = u'6个月'
+        price = 600
+    pay_url = create_direct_pay_by_user(order_id,name,desc,price)
+    print pay_url
+    #return HttpResponseRedirect(pay_url)
+     
