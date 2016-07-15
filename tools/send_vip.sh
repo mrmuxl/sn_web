0ab021276de8e38bcae2913d32ec0fa909959737
xulin <mrmuxl@sina.com>
2016-07-15 23:45:50 +0800

    json 顺序 	修改：      apps/publish/utils.py

--- tools/send_vip.sh
+++ tools/send_vip.sh
@@ -2,6 +2,6 @@
 for i in `cat f.txt`
 do
     echo $i
-    curl http://www.simplenect.cn/vipuser/vipuser_test/518279d14e20e/$i
+    curl http://localhost:8000/vipuser/vipuser_test/518279d14e20e/$i
     sleep 2
 done
