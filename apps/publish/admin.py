de3f94bcf7d0e2627c7deb68e9af870bd91fa8df
xulin <mrmuxl@sina.com>
2016-07-18 20:10:24 +0800

    修改：      apps/publish/admin.py 	修改：      apps/vipuser/mail_text.py 	修改：      apps/vipuser/views.py

--- apps/publish/admin.py
+++ apps/publish/admin.py
@@ -13,8 +13,27 @@ class PublishUserAdmin(admin.ModelAdmin):
     fieldsets = (
         (u'添加用户', {
             'classes': ('wide',),
-            'fields': ('email', 'ver', 'repo_ver')}
+            'fields': ('email', 'ver', 'repo_ver','is_publish')}
         ),
     )
+    actions = ['make_published','make_unpublished']
+    def make_published(self, request, queryset):
+        rows_updated = queryset.update(is_publish=True)
+        if rows_updated == 1:
+            message_bit = "1 个用户"
+        else:
+            message_bit = "%s 个用户" % rows_updated
+        self.message_user(request, u"%s 成功标记为已发布。" % message_bit)
+    make_published.short_description = u"标记所选的用户为已发布"
+
+    def make_unpublished(self, request, queryset):
+        print queryset
+        rows_updated = queryset.update(is_publish=False)
+        if rows_updated == 1:
+            message_bit = u"1 个用户"
+        else:
+            message_bit = u"%s 个用户" % rows_updated
+        self.message_user(request, u"%s 成功标记为未发布。" % message_bit)
+    make_unpublished.short_description = u"标记所选的用户为未发布"
 
 admin.site.register(PublishUser,PublishUserAdmin)
