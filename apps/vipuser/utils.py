23fb554d91df200240995005271442551ba78693
xulin <mrmuxl@sina.com>
2016-09-01 10:33:01 +0800

    修改：      utils.py

--- apps/vipuser/utils.py
+++ apps/vipuser/utils.py
@@ -30,10 +30,11 @@ def access_user_print(email):
                     print_access_list.append(i.access_user_id)
                     #print_access_list.append({"email":i.access_user_id})
             message['print_access_user']=print_access_list
-            logger.info("access_user_print:%s",message)
+            logger.info("access_user_print_message:%s",message)
     except Exception as e:
         logger.debug("print_user:%s",e)
         print_access_user_set = Print.objects.filter(print_access__access_user=email).filter(expire__gt=now).filter(print_access__status=1)
+        logger.info("print_access:%s",print_access_user_set)
         is_print =False
         if print_access_user_set:
             for i in print_access_user_set:
@@ -42,7 +43,7 @@ def access_user_print(email):
         message['is_print']= is_print 
         message['remainder_print_num']= -1
         message['print_access_user']=print_access_list
-        logger.info("print_access_user_set:%s",message)
+        logger.info("print_access_user_set_message:%s",message)
     return message
 
 def access_user_shared(email):
@@ -60,7 +61,7 @@ def access_user_shared(email):
                     shared_access_list.append(i.access_user_id)
                     #shared_access_list.append({"email":i.access_user_id})
             message['shared_access_user']=shared_access_list
-            logger.info("access_user_shared:%s",message)
+            logger.info("access_user_shared_message:%s",message)
     except Exception as e:
         logger.debug("shared_user:%s",e)
         shared_access_user_set = Shared.objects.filter(shared_access__access_user=email).filter(expire__gt=now).filter(shared_access__status=1)
@@ -73,6 +74,6 @@ def access_user_shared(email):
         message['is_shared']= is_shared 
         message['remainder_shared_num']= -1
         message['shared_access_user']=shared_access_list
-        logger.info("shared_access_user_set:%s",message)
+        logger.info("shared_access_user_set_message:%s",message)
     return message
  
