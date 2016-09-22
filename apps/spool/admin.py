fef28cc7b6f9d878fb22e1c17bcaefef0bb256ee
xulin <mrmuxl@sina.com>
2016-09-22 18:19:31 +0800

    修改：      apps/spool/admin.py

--- apps/spool/admin.py
+++ apps/spool/admin.py
@@ -6,5 +6,5 @@ from forms import SpoolForm
 
 class SpoolAdmin(admin.ModelAdmin):
     spoolform = SpoolForm
-    list_filter = ('uuid','origin_email','origin_uuid','accept_email','accept_uuid','print_name','print_uuid','file_name','file_path','file_client_path','page_num','print_time','create_at','status','status_time')
+    list_filter = ('uuid','origin_email','origin_uuid','accept_email','accept_uuid','printer_name','printer_uuid','file_name','file_path','file_client_path','page_num','print_time','create_at','status','status_time')
 admin.site.register(Spool,SpoolAdmin)
