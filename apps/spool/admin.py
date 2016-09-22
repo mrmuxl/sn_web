04260d3fd71a68566e8d291e9488dc5ac6797d7e
xulin <mrmuxl@sina.com>
2016-09-22 21:26:12 +0800

    -list_display =      ('uuid','origin_email','origin_uuid','accept_email','accept_uuid','printer_name','printer_uuid','file_name','file_path','file_client_path','page_num' +    list_display =      ('uuid','origin_email','accept_email','printer_name','file_name','page_num','print_time','create_at','status','status_time') +    list_filter = ('status',)

--- apps/spool/admin.py
+++ apps/spool/admin.py
@@ -6,5 +6,6 @@ from forms import SpoolForm
 
 class SpoolAdmin(admin.ModelAdmin):
     spoolform = SpoolForm
-    list_display = ('uuid','origin_email','origin_uuid','accept_email','accept_uuid','printer_name','printer_uuid','file_name','file_path','file_client_path','page_num','print_time','create_at','status','status_time')
+    list_display = ('uuid','origin_email','accept_email','printer_name','file_name','page_num','print_time','create_at','status','status_time')
+    list_filter = ('status',)
 admin.site.register(Spool,SpoolAdmin)
