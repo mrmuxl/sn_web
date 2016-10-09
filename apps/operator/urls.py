91325f2557bbcc93e2de3b70f89ca21ed1a26185
xulin <mrmuxl@sina.com>
2016-10-10 03:05:44 +0800

    修改：      apps/ad/admin.py 	修改：      apps/ad/forms.py 	重命名：    apps/operator/models.py -> apps/ad/models.py 	修改：      apps/ad/urls.py 	修改：      apps/ad/views.py 	修改：      apps/kx/admin.py 	删除：      apps/operator/__init__.py 	删除：      apps/operator/admin.py 	删除：      apps/operator/forms.py 	删除：      apps/operator/urls.py 	删除：      apps/operator/views.py 	修改：      config/dev.py 	修改：      templates/classic/ad/operator_add_form.html

--- apps/operator/urls.py
+++
@@ -1,8 +0,0 @@
-#_*_coding:utf-8_*_
-from django.conf.urls import patterns, include, url
-from django.views.generic import DetailView, ListView
-
-urlpatterns = patterns('',
-    url(r'^$','apps.operator.views.index',name ='operator_index'),
-)
-
