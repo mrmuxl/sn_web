72a61b7ddefdb2e2dc510661a730137ee99ee76f
mokeming <mokeming@163.com>
2016-10-30 17:43:28 +0800

    json 返回中文

--- apps/utils/json_util.py
+++ apps/utils/json_util.py
@@ -1,4 +1,4 @@
 import json
 from django.http import HttpResponse
 def json_return(json_data,ensure_ascii=True):
-	return HttpResponse(json.dumps(json_data,ensure_ascii),content_type="application/json")
+	return HttpResponse(json.dumps(json_data,ensure_ascii=ensure_ascii),content_type="application/json")
